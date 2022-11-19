cda540be976ea65672dab437d84985e9040b5c56
Denis Kalbasov <denis.kalbasov@yandex.ru>
2022-11-19 14:23:23 +0100

    utils/wait_for_ backoff config changed

--- fastapi-solution/tests/functional/utils/config.py
+++ fastapi-solution/tests/functional/utils/config.py
@@ -1,5 +1,4 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
 from elasticsearch.exceptions import ConnectionError as ConErr
 from aioredis.exceptions import ConnectionError
@@ -8,7 +7,7 @@ from aioredis.exceptions import ConnectionError
 LOGGER_SETTINGS = {
     "format": "%(asctime)s - %(name)s.%(funcName)s:%(lineno)d - %(levelname)s - %(message)s",
     "datefmt": "%Y-%m-%d %H:%M:%S",
-    "handlers": [RotatingFileHandler('logs.log', maxBytes=2000000, backupCount=2)],
+    "handlers": None,
     "level": logging.INFO
 }
 
