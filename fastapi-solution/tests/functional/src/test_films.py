9d9915d902717f4382c30526653cbb925565d01c
Denis Kalbasov <denis.kalbasov@yandex.ru>
2022-11-20 12:02:15 +0100

    test_film_sort

--- fastapi-solution/tests/functional/src/test_films.py
+++ fastapi-solution/tests/functional/src/test_films.py
@@ -1,40 +1,11 @@
-# import datetime
-# import uuid
 import json
 from http import HTTPStatus
 
 import aiohttp
 import pytest
 
-
 from ..settings import test_settings
 from .common import make_bulk_query
-
-
-# def get_es_films_bulk_query(query_data, es_index, es_id_field, items_count):
-#     # 1. Генерируем данные для ES
-#     es_data = [{
-#         'id': str(uuid.uuid4()),
-#         'imdb_rating': 8.5,
-#         'genre': ['Action', 'Sci-Fi'],
-#         'title': query_data['search'],
-#         'description': 'New World',
-#         'director': ['Stan'],
-#         'actors_names': ['Ann', 'Bob'],
-#         'writers_names': ['Ben', 'Howard'],
-#         'actors': [
-#             {'id': '111', 'name': 'Ann'},
-#             {'id': '222', 'name': 'Bob'}
-#         ],
-#         'writers': [
-#             {'id': '333', 'name': 'Ben'},
-#             {'id': '444', 'name': 'Howard'}
-#         ],
-#         'created_at': datetime.datetime.now().isoformat(),
-#         'updated_at': datetime.datetime.now().isoformat(),
-#         'film_work_type': 'movie'
-#     } for _ in range(items_count)]
-#     return make_bulk_query(es_data, es_index, es_id_field)
 
 
 @pytest.mark.parametrize(
@@ -51,13 +22,13 @@ from .common import make_bulk_query
     ]
 )
 @pytest.mark.asyncio
-async def test_film(es_write_data, redis_client, checking_id, expected_answer):
+async def test_film(es_write_data, redis_client, checking_id, expected_answer, make_id_request):
 
     # 1. Генерируем данные для ES
     film_id = '7e0ad51a-332f-4ff0-b8b9-9b5308836cb1'
     es_data = [{
         'id': film_id,
-        'imdb_rating': 8.5,
+        'imdb_rating': 9.5,
         'genre': ['Action', 'Sci-Fi'],
         'title': 'The Star',
         'description': 'New World',
@@ -100,3 +71,22 @@ async def test_film(es_write_data, redis_client, checking_id, expected_answer):
         assert cache_data == body
     else:
         assert cache_data_str is None
+
+
+@pytest.mark.parametrize(
+    'expected_answer',
+    [
+        {'raiting': 9.5}
+    ]
+)
+@pytest.mark.asyncio
+async def test_film_sort(expected_answer, make_search_request):
+
+    session = aiohttp.ClientSession()
+    url = f'http://{test_settings.service_host}:8000/api/v1/films/search/?query=star&sort=imdb_rating'
+    async with session.get(url) as response:
+        body = await response.json()
+        result = body[0].get('imdb_rating')  # проверяем первый элмент, первым должен находится фильм с самым высоким рейтингом
+    await session.close()
+
+    assert result == expected_answer['raiting']
